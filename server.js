f0d14b44868f71bbfe7b0729347a47f02bb84dd8
seanmckee <seanmingmckee@gmail.com>
2023-02-13 09:58:41 -0800

    register user complete

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
+
+// Connect DB
+connectDB()
+
+// init middleware
+app.use(express.json({ extended: false }))
 
 const PORT = process.env.PORT || 5000
 
